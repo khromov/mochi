bd042f4f7a386039347f1441034d6609e46fc688
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-06-07 23:56:33 +0200

    chore: reduce base docker image size

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,7 +4,7 @@ set -e
 IMAGE_NAME="mochi"
 
 echo "Building Docker image..."
-docker build -t "$IMAGE_NAME" .
+docker build -f Dockerfile.production -t "$IMAGE_NAME" .
 
 echo "Dropping into shell..."
 docker run --rm -it -p 3333:3333 --entrypoint /bin/sh "$IMAGE_NAME"
