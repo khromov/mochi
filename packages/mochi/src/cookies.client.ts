f54091449cbd88327b267d6eb5e4651483f1811c
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-07-01 15:42:34 +0200

    chore: misc tweaks (#135)
    * Build cache
    * Bump svelte-shaker
    * Vendor js-cookie
    * Helper scripts

--- packages/mochi/src/cookies.client.ts
+++ packages/mochi/src/cookies.client.ts
@@ -1,4 +1,4 @@
-import Cookies from 'js-cookie';
+import Cookies from './vendor/js-cookie/index.ts';
 import type { CookieSerializeOptions, Cookie } from './cookies';
 
 export function createClientCookies() {
@@ -17,11 +17,11 @@ export function createClientCookies() {
     },
 
     set(name: string, value: string, options?: CookieSerializeOptions): void {
-      Cookies.set(name, value, options);
+      Cookies.set(name, value, options ? { ...options } : undefined);
     },
 
     delete(name: string, options?: Pick<CookieSerializeOptions, 'path' | 'domain'>): void {
-      Cookies.remove(name, options);
+      Cookies.remove(name, options ? { ...options } : undefined);
     },
   };
 }
